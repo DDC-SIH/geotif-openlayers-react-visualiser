ea435d8176cffe99d66a585f10f69fc93a51f0fd
Subinoy Biswas <115915591+subinoybiswas@users.noreply.github.com>
2024-12-03 12:03:14 +0530

    add: band arithmatic

--- src/components/MapArea.tsx
+++ src/components/MapArea.tsx
@@ -268,7 +268,66 @@ const GeoTIFFMap = () => {
         const layer = new TileLayer({
             className: "tiff",
             source: geoTIFFSource,
-           
+            style: {
+                color: [
+                    'case',
+                    ['all',
+                        ['>', ['band', 1], 0],  // Check if band 1 has valid data
+                        ['>', ['band', 2], 0],  // Check if band 2 has valid data
+                        ['>', ['band', 3], 0]   // Check if band 3 has valid data
+                    ],
+                    [
+                        'interpolate',
+                        ['linear'],
+                        // calculate NDVI, bands come from the sources below
+                        ['/', ['-', ['band', 3], ['band', 2]], ['+', ['band', 3], ['band', 2]]],
+                        // color ramp for NDVI values, ranging from -1 to 1
+                        -0.2,
+                        [191, 191, 191],
+                        -0.1,
+                        [219, 219, 219],
+                        0,
+                        [255, 255, 224],
+                        0.025,
+                        [255, 250, 204],
+                        0.05,
+                        [237, 232, 181],
+                        0.075,
+                        [222, 217, 156],
+                        0.1,
+                        [204, 199, 130],
+                        0.125,
+                        [189, 184, 107],
+                        0.15,
+                        [176, 194, 97],
+                        0.175,
+                        [163, 204, 89],
+                        0.2,
+                        [145, 191, 82],
+                        0.25,
+                        [128, 179, 71],
+                        0.3,
+                        [112, 163, 64],
+                        0.35,
+                        [97, 150, 54],
+                        0.4,
+                        [79, 138, 46],
+                        0.45,
+                        [64, 125, 36],
+                        0.5,
+                        [48, 110, 28],
+                        0.55,
+                        [33, 97, 18],
+                        0.6,
+                        [15, 84, 10],
+                        0.65,
+                        [0, 69, 0],
+
+                    ],
+                    [0, 0, 0, 0]  // Transparent for no data
+                ]
+            }
+
         });
 
         setTiffLayer(layer);
